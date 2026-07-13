524be1f3ff6304332909756c37cc89d5aee6f540
j4ckalll <jishnuvinodgk@gmail.com>
2026-07-13 22:17:20 +0530

    age

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int age = 18;
+    double grade = 94.5;
+
+    // Printing simple text
+    std::cout << "Hello!" << std::endl;
+
+    // Printing variables mixed with text
+    std::cout << "I am " << age << " years old." << std::endl;
+    std::cout << "My final grade is: " << grade << std::endl;
+
     return 0;
 }
