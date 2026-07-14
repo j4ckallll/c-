f8f1ceaf66e8afd899e5163b9a39be406551a04e
j4ckalll <jishnuvinodgk@gmail.com>
2026-07-14 19:57:43 +0530

    multi line cout

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
-
-int main() {
-    int age = 18;
-    double grade = 94.5;
-
-    // Printing simple text
-    std::cout << "Hello!" << std::endl;
-
-    // Printing variables mixed with text
-    std::cout << "I am " << age << " years old." << std::endl;
-    std::cout << "My final grade is: " << grade << std::endl;
-
-    return 0;
+using namespace std;
+int main()
+{
+  cout<<"Hi\nthis is \njishnu";
 }
