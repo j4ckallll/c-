da12a71599abace8e48a6283a5c21cdea3665aef
j4ckallll <jishnuvinodgk@gmail.com>
2026-07-31 22:04:20 +0530

    weak

--- main.cpp
+++ main.cpp
@@ -3,22 +3,26 @@ using namespace std;
 
 int main() 
 {
-  int age;
-  cout << "Enter your age: " << endl;
-  cin >> age;
-  
-  if (age > 60)
-  {
-    cout << "You are overaged" << endl;
-  }
-  else if (age >= 18)
-  {
-    cout << "You are eligible to vote";
-  }
-  else
-  {
-    cout << "You are not eligible to vote" << endl;
-  }
-  
-  return 0;
+
+
+    int day = 2;
+
+    if (day == 1)
+        cout << "Sunday" << endl;
+    else if (day == 2)
+        cout << "Monday" << endl;
+    else if (day == 3)
+        cout << "Tuesday" << endl;
+    else if (day == 4)
+        cout << "Wednesday" << endl;
+    else if (day == 5)
+        cout << "Thursday" << endl;
+    else if (day == 6)
+        cout << "Friday" << endl;
+    else if (day == 7)
+        cout << "Saturday" << endl;
+    else
+        cout << "Invalid day number" << endl;
+
+    return 0;
 }
