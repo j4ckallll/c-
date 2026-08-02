c1ac92449874fd5c4da41b847b416bb224ff7408
j4ckallll <jishnuvinodgk@gmail.com>
2026-08-02 22:02:56 +0530

    while loop

--- main.cpp
+++ main.cpp
@@ -3,36 +3,13 @@ using namespace std;
 
 int main() 
 {
-    int month;
-    cout << "Enter a month number (1-12): ";
-    cin >> month;
+    int count = 1;
 
-    if (month == 1)
-        cout << "January" << endl;
-    else if (month == 2)
-        cout << "February" << endl;
-    else if (month == 3)
-        cout << "March" << endl;
-    else if (month == 4)
-        cout << "April" << endl;
-    else if (month == 5)
-        cout << "May" << endl;
-    else if (month == 6)
-        cout << "June" << endl;
-    else if (month == 7)
-        cout << "July" << endl;
-    else if (month == 8)
-        cout << "August" << endl;
-    else if (month == 9)
-        cout << "September" << endl;
-    else if (month == 10)
-        cout << "October" << endl;
-    else if (month == 11)
-        cout << "November" << endl;
-    else if (month == 12)
-        cout << "December" << endl;
-    else
-        cout << "Invalid month number" << endl;
+    while (count <= 5) 
+    {
+        cout << "Count is: " << count << endl;
+        count++; 
+    }
 
     return 0;
 }
