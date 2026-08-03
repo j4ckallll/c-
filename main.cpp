4042a181b2bd401b184e792b9db129c6a8f0a1e1
j4ckallll <jishnuvinodgk@gmail.com>
2026-08-03 22:01:39 +0530

    nested loop"

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() 
 {
-    int count = 1;
-
-    while (count <= 5) 
+    for (int i = 1; i <= 3; i++) 
     {
-        cout << "Count is: " << count << endl;
-        count++; 
+        for (int j = 1; j <= 3; j++) 
+        {
+            cout << i << j << " ";
+        }
+        cout << endl;
     }
 
     return 0;
