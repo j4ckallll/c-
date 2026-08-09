fec486ab88d590b02fcdefc029861a8a0784727f
j4ckallll <jishnuvinodgk@gmail.com>
2026-08-09 21:18:12 +0530

    2d array

--- main.cpp
+++ main.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main() {
 
-    int ages[5] = {21, 22, 21, 34, 45};
+    int D2Array[5][5] = {
+        {21, 22, 21, 34, 45},
+        {21, 22, 21, 34, 45},
+        {21, 22, 21, 34, 45},
+        {21, 22, 21, 34, 45},
+        {21, 22, 21, 34, 45}
+    };
 
     for (int i = 0; i < 5; i++) {
-        cout << ages[i] << " ";
+        for (int j = 0; j < 5; j++) {
+            cout << D2Array[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
 
     return 0;
 }
