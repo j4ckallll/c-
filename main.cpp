f19ed354e758322a8ea5c4c1b3def6826ef90b16
j4ckallll <jishnuvinodgk@gmail.com>
2026-08-15 19:37:41 +0530

    default arg

--- main.cpp
+++ main.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-int after5(int, int, string); 
-
-int main() {
-    int age;
-    age = 24;
-
-    cout << after5(age, 2, "Hello") << endl; 
-    cout << after5(20, 5, "Hi") << endl;
-
-    return 0;
+int add(int num=3)
+{
+  cout<<num+2<<endl;
+}
+int main()
+{
+  add(8);
+  add();
+  return 0;
 }
 
-
-int after5(int a, int b, string greeting) {
-    
-    return a + b;
-}
