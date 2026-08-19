f592f35d14118a71d7f7d590bfccb07bd3f03627
j4ckallll <jishnuvinodgk@gmail.com>
2026-08-19 22:27:12 +0530

    string

--- main.cpp
+++ main.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
-class student {
-    public:
-        int roll_no;
-        string name;
-};
-
-int main() {
-
-    student jishnu;
-    jishnu.roll_no = 1;
-    jishnu.name = "jishnu";
-
-    student vishnu;
-    vishnu.roll_no = 2;
-    vishnu.name = "vishnu";
-
-    cout<<jishnu.name<<endl;
-
-    return 0;
+int main()
+{
+  string jishnu="jishnu",del;
+  jishnu=jishnu+"v";
+ 
+  string vishnu="vishnu"; 
+  cout<<vishnu<<" v";
 }
